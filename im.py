69eaf310dcf60b438b4681459bfb936e740f44f9
eeghor <eeghor@gmail.com>
2016-10-03 22:23:54 +1100

    shorter version

--- im.py
+++ im.py
@@ -1,27 +1,29 @@
-import pandas as pd  # to use data frames
-import sys  # for command line argument(s)
-import re  # search for something
+"""
+The IMDB 5000 Movie Database pre-processing. Additional data scraped from imdb.com.
+---
+usage: python3 im.py [data file]
+"""
+
+import pandas as pd  
+import sys  		 
+import re  			 # search for something in a string
 from string import printable  # remove unprintable characters
-import requests  # html scraping
-from bs4 import BeautifulSoup  # scraping
+import requests  	 # html scraping
+from bs4 import BeautifulSoup  # HTML parsing
 import time
 
-# mvs = re.compile(r"\"(?P<title>.*?)\"\s+\((?P<year>199[6-9]+|2\d{2}6)\)\s+(?P<country>\w+):(?P<r_date>.*\d{4})")
-# #mvs2 = re.compile(r"\"(?P<title>.*?)\"\s+\((?P<year>201[4-5]{1})\)\s+(?P<genre>\w+)")
+first_relevant_year = 1991  # only consider movies released since this year
 
-first_relevant_year = 1991
-
-fl = sys.argv[1]  # movie database file
-fl_upd = fl.split(".").insert(1,"_upd.")  # file to store processed data
-print(fl_upd)
+fl = sys.argv[1]  # movie database file; assume that fl has the form [name].[extension]
+fl_upd = fl.split(".")[0]+"_upd.csv" # file to store processed data 
 
 start_time = time.time()
-df = pd.read_csv(fl, encoding="utf-8")
+
+df = pd.read_csv(fl, encoding="utf-8", dtype={"title_year": float})
 
 print("found {} records (movies) in the database file".format(len(df.index)))
 
-# we only need the movies released since 1991
-df = df[df["title_year" >= first_relevant_year]]
+df = df[df["title_year"] >= first_relevant_year]
 ndf = len(df.index)
 print("removed movies released prior to {}; now have {} movies left".format(first_relevant_year, ndf))
 
@@ -31,9 +33,9 @@ df["movie_title"] = df["movie_title"].apply(lambda _: "".join([ch for ch in _ if
 # to extract release date
 rd = re.compile(r"(\d\d?\s+\w+\s+\d{4})")
 
-# lists to collect.. 
-rd_list  = []  # release dates
-tg_list  = []  # taglines
+# lists to collect new data in
+rd_list  = [None]*ndf  # release dates
+tg_list  = [None]*ndf  # taglines
 
 
 for g, link in enumerate(df["movie_imdb_link"],1):
@@ -46,24 +48,17 @@ for g, link in enumerate(df["movie_imdb_link"],1):
 	for line in soup.find_all('h4'):
 		if line.text == "Release Date:":
 			try:
-				rd_list.append(re.search(rd,line.next_sibling.strip()))
-			except TypeError:
-				rd_list.append(None)
+				rd_list[g-1] = re.search(rd,line.next_sibling.strip()).group(0)
+			except:
+				pass
 		elif line.text == "Taglines:":
-				tagl = line.next_sibling.strip()
-				if len(tagl) > 0:
-					tg_list.append(tagl)
-				else:
-					tg_list.append(None)
-
-print("found {} release dates and {} taglines".format(len(rd_list), len(tg_list)))
+				tg_list[g-1] = line.next_sibling.strip()
 
 df["rel_date"] = rd_list
 df["tagline"] = tg_list
 
-print("saving the updated database to {}...".format(fl_upd))
 df.to_csv(fl_upd)
 
 end_time = time.time()
 
-print("done. elapsed time is {} minutes".format(round((end_time - start_time)/60,1)))
+print("done. saved data to {}; elapsed time is {} minutes".format(fl_upd, round((end_time - start_time)/60,1)))
